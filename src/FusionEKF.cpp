1ee857290065d23f362e44df5a1da15cdf70b50e
Charing Chen <charinghaxen@gmail.com>
2020-09-01 17:53:34 -0700

    Add Radar and lidar choosing logic
    Ready to connect to the simulator and debug

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -158,9 +158,19 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // TODO: Radar updates
+      // Calculate jacobian from x_
+      Hj_ = tools.CalculateJacobian(ekf_.x_);
+      // Assign new Hj to H, and R to radar R
+      ekf_.H_ = Hj_;
+      ekf_.R_ = R_radar_;
+      // perform extend kalman fitler from ekf_ object
+      ekf_.UpdateEKF(measurement_pack.raw_measurements_);
 
   } else {
     // TODO: Laser updates
+      ekf_.H_ = H_laser_;
+      ekf_.R_ = R_radar_;
+      ekf_.Update(measurement_pack.raw_measurements_);
 
   }
 
